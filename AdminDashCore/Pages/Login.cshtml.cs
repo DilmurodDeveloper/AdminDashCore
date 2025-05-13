cbe2bdd80a87e62b2927845b454f70f631f25427
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-05-13 11:31:12 +0500

    FEAT: Implemented static login check for faster authentication

--- AdminDashCore/Pages/Login.cshtml.cs
+++ AdminDashCore/Pages/Login.cshtml.cs
@@ -2,36 +2,32 @@ using AdminDashCore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AdminDashCore.Pages;
-
-public class LoginModel : PageModel
+namespace AdminDashCore.Pages
 {
-    [BindProperty]
-    public User? LoginUser { get; set; }
-
-    public string? ErrorMessage { get; set; }
-
-    public void OnGet()
+    public class LoginModel : PageModel
     {
-        HttpContext.Session.Clear();
-    }
+        [BindProperty]
+        public User? LoginUser { get; set; }
 
-    public IActionResult OnPost()
-    {
-        if (!ModelState.IsValid)
+        public string? ErrorMessage { get; set; }
+
+        public void OnGet()
         {
+            HttpContext.Session.Clear();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (LoginUser?.Username == "admin" && LoginUser.Password == "123456")
+            {
+                HttpContext.Session.SetString("IsLoggedIn", "true");
+                HttpContext.Session.SetString("Username", LoginUser.Username);
+
+                return RedirectToPage("/Admin/Dashboard");
+            }
+                
+            ErrorMessage = "The username or password is incorrect.";
             return Page();
         }
-
-        if (LoginUser?.Username == "admin" && LoginUser.Password == "123456")
-        {
-            HttpContext.Session.SetString("IsLoggedIn", "true");
-            HttpContext.Session.SetString("Username", LoginUser.Username);
-
-            return RedirectToPage("/Admin/Dashboard");
-        }
-
-        ErrorMessage = "The username or password is incorrect.";
-        return Page();
     }
 }
